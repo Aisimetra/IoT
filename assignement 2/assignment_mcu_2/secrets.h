c18c7e1442dc994854919195dac49787d36d376c
Artemisia Sarteschi <a.sarteschi@campus.unimib.it>
2022-05-17 15:27:56 +0200

    MCU assignement 2

---
+++ assignement 2/assignment_mcu_2/secrets.h
@@ -0,0 +1,19 @@
+// Use this file to store all of the private credentials and connection details
+
+// MQTT access
+#define MQTT_BROKERIP "149.132.178.180"           // IP address of the machine running the MQTT broker
+#define MQTT_CLIENTID "asarteschi16"                 // client identifier
+#define MQTT_USERNAME "asarteschi"            // mqtt user's name
+#define MQTT_PASSWORD "iot829677"            // mqtt user's password
+
+// WiFi configuration
+#define SECRET_SSID "IoTLabThingsU14"                  // SSID
+#define SECRET_PASS "L@b%I0T*Ui4!P@sS**0%Lessons!"              // WiFi password
+
+// ONLY if static configuration is needed
+/*
+#define IP {192, 168, 1, 100}                    // IP address
+#define SUBNET {255, 255, 255, 0}                // Subnet mask
+#define DNS {149, 132, 2, 3}                     // DNS
+#define GATEWAY {149, 132, 182, 1}               // Gateway
+*/
